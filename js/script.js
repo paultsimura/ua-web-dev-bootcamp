d018db424215e2300b0cce8ab18f55cbe2c80547
paultsimura <paultsimura@corp.doosan.com>
2020-04-19 13:48:36 +0300

    Add scroll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
-$(function () {
-  $(window).on('scroll', function () {
-    if ( $(window).scrollTop() > 10 ) {
-      $('.navbar').addClass('active');
-    } else {
-      $('.navbar').removeClass('active');
+(function($) {
+  'use strict'; // Start of use strict
+
+  const OFFSET = 60;
+
+  // Add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#main-navbar',
+    offset: OFFSET
+  });
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      let target = $(this.hash);
+      target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - (OFFSET - 2))
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
     }
   });
-});
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Collapse Navbar
+  const activateNavbar = function() {
+    let $mainNavbar = $("#main-navbar");
+    if ($mainNavbar.offset().top > 100) {
+      $mainNavbar.addClass('active');
+    } else {
+      $mainNavbar.removeClass('active');
+    }
+  };
+  // Collapse now if page is not at top
+  activateNavbar();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(activateNavbar);
+})(jQuery); // End of use strict
